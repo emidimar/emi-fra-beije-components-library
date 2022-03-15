c984d929213a7b3498f099a8d75df61f8e2ac555
emidimar <dimariemilio96@gmail.com>
2022-03-15 15:53:39 +0100

    start project

---
+++ src/components/CustomButton.js
@@ -0,0 +1,11 @@
+import { View, Text, Button } from 'react-native-web'
+import React from 'react'
+
+export default function CustomButton() {
+    return (
+        <View>
+            <Text>Bottone</Text>
+            <Button />
+        </View>
+    )
+}
